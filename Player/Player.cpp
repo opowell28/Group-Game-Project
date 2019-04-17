f3b36922e9ffda49050b412512e322bbeafc2a81
Owen Powell <opowell2899@gmail.com>
2019-04-17 19:42:26 -0400

    Edited functions for readability

--- Player/Player.cpp
+++ Player/Player.cpp
@@ -11,7 +11,7 @@ Player::Player() {
     this->carryCapacity = 0.0;
 }
 
-//player object with parameters
+//player object with set parameters
 Player::Player(std::string playerName, int playerHealth, double carryCapacity) {
     this->playerName = playerName;
     this->playerHealth = playerHealth;
@@ -23,44 +23,44 @@ double Player::getCarryCapacity(double carryCapacity) {
     carryCapacity = this->carryCapacity;
     return carryCapacity;
 }
-/*
-int Player::attack() {
+
+
+int Player::playerAttack() {
+    //holds the input from the player when they want to attack
+    std::string atkStrength;
+    getline(std::cin, atkStrength);
+
+    //holds the integer value of damage to be dealt to the character
     int atkDamage;
-    //generate number between 1 and 5 for light damage
+
+    //generate number between 1 and 3 for light damage
     if (atkStrength == "light" || atkStrength == "Light") {
         srand((unsigned)time(0));
         for(int index = 0; index < 1; index++) {
-            atkDamage = (rand() % 5) + 1;
+            atkDamage = (rand() % 3) + 1;
             return atkDamage;
         }
+        characterHealth -= atkDamage;
+        return characterHealth;
     }
-    //generate random number between 6 and 10 for heavy damage
+        //generate random number between 8 and 10 for heavy damage
     else if (atkStrength == "heavy" || atkStrength == "Heavy") {
-        srand((unsigned)time(0));
-        for(int index = 0; index < 1; index++) {
-            atkDamage = (rand() % 10) + 6;
+        srand((unsigned) time(0));
+        for (int index = 0; index < 1; index++) {
+            atkDamage = (rand() % 10) + 8;
             return atkDamage;
         }
+        characterHealth -= atkDamage;
+        return characterHealth;
+
+    } else {
+
+        srand((unsigned) time(0));
+        for (int index = 0; index < 1; index++) {
+            atkDamage = (rand() % 7) + 4;
+            return atkDamage;
+        }
+        characterHealth -= atkDamage;
+        return characterHealth;
     }
 }
- */
-
-//ask user what strength of attack to perform
-std::string Player::atkRequest() {
-    std::cout << "Heavy or light attack? Enter 'heavy' or 'light' " << std::endl;
-    getline(std::cin, this->atkStrength);
-    return atkStrength;
-}
-
-//damage function which lowers players health
-/*
-int Player::playerDamaged() {
-    int playerDamage;
-    //removes between 1 and 10 from player's health
-    srand((unsigned)time(0));
-    for(int index = 0; index < 1; index++) {
-        playerDamage = (rand() % 10) + 1;
-        return playerDamage;
-    }
-}
- */
