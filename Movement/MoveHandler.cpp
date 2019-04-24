9d5b9d8f287550aee2bf765b78e7ab6779b5fb8f
Joshua <pwr2stand@gmail.com>
2019-04-24 13:31:53 -0400

    adding story elements

--- Movement/MoveHandler.cpp
+++ Movement/MoveHandler.cpp
@@ -364,7 +364,55 @@ void MoveHandler::RunStoryEvent(int x, int y, CombatHandler *CombatHndlr, Player
             }
 
         } else if ((x == 0) && (y == 0)) {
+            printStory("This room has only one exit besides the one you came through.");
+        } else if ((x == 0) && (y == 1)) {
 
+        } else if ((x == 1) && (y == 1)) {
+
+        } else if ((x == 3) && (y == 1)) {
+
+        } else if ((x == 2) && (y == 2)) {
+
+        } else if ((x == 4) && (y == 2)) {
+
+        } else if ((x == 1) && (y == 3)) {
+
+        } else if ((x == 3) && (y == 3)) {
+
+        } else if ((x == 0) && (y == 4)) {
+
+        } else if ((x == 2) && (y == 4)) {
+
+        } else if ((x == 1) && (y == 5)) {
+
+        } else if ((x == 4) && (y == 5)) {
+
+        } else if ((x == 3) && (y == 6)) {
+
+        } else if ((x == 0) && (y == 6)) {
+
+        } else if ((x == 1) && (y == 7)) {
+
+        } else if ((x == 2) && (y == 7)) {
+
+        } else if ((x == 3) && (y == 8)) {
+
+        } else if ((x == 4) && (y == 8)) {
+
+        } else if ((x == 2) && (y == 9)) {
+
+        } else if ((x == 1) && (y == 9)) {
+
+        } else if ((x == 0) && (y == 10)) {
+            printStory("You cannot go forward anymore, but you can see faint light coming from your left.");
+        } else if ((x == 1) && (y == 10)) {
+            printStory("You see light coming through the opening to your right. You can also hear something moving around in there. Something big.");
+        } else if ((x == 2) && (y == 10)) {   //BOSS ROOM
+
+        } else if ((x == 3) && (y == 10)) {
+            printStory("You see light coming through the opening to your left. You can also hear something moving around in there. Something big.");
+        } else if ((x == 4) && (y == 10)) {
+            printStory("You cannot go forward anymore, but you can see faint light coming from your right.");
         } else {
             cout << "There is nothing interesting about this room." << endl;
         }
